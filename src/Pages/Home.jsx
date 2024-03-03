b95754e17ac636897f64bd8c65579c3fca6cb285
Himanshu singh <123050257+DevHimanshu630@users.noreply.github.com>
2024-03-03 20:30:10 +0530

    certificate page updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -68,7 +68,7 @@ function Home() {
           className="overflow-y-auto overflow-x-hidden fixed top-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
         >
           {/* Modal content */}
-          <div className="relative p-4 w-full max-w-2xl max-h-full">
+          <div className="relative p-4 w-full max-w-2xl max-h-full border">
             <div className="relative bg-white dark:bg-white">
               <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                 <h3 className="text-xl font-semibold text-gray-900 dark:text-black">
